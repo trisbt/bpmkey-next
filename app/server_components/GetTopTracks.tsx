35d5345486f4993dc4e586aea68578651fcc86e4
trisbt <tristanbott30@gmail.com>
2023-10-27 16:29:13 -0700

    remove revalidate for testing

--- app/server_components/GetTopTracks.tsx
+++ app/server_components/GetTopTracks.tsx
@@ -7,19 +7,21 @@ import { GetTracksItem, TopTracksItem } from "../types/serverTypes";
 //revalidate every half week
 
 const GetTopTracks = async () => {
-    const token = await GetAccessToken(302400);
+    const token = await GetAccessToken();
 
     const res = await fetch(`https://api.spotify.com/v1/playlists/37i9dQZEVXbNG2KDcFcKOF?si=ce928cdd687a4612/tracks`, {
         headers: {
             'Authorization': 'Bearer ' + token
         },
-        next: {
-            revalidate: 302400,
-        }
+    
        
     });
-
+    // if (!res.ok) {
+    //     throw new Error(`API response failed with status ${res.status}: ${res.statusText}`);
+    // }
+    
     const data = await res.json();
+    // console.log(data);
     const trackData = data.tracks.items.slice(0, 10).map((item: TopTracksItem) => item.track);
     const trackID = data.tracks.items.slice(0, 10).map((item: TopTracksItem) => item.track.id);
 

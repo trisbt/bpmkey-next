af8b1b7a0192f31cd484cc360bd7754dc05eb0d2
trisbt <tristanbott30@gmail.com>
2023-10-27 16:08:53 -0700

    test loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,22 +2,20 @@ import { CircularProgress, Box } from "@mui/material";
 
 export default function Loading() {
     return (
-        <div className='song-page-main background-gradient min-h-[150em] min-w-[100vw]'>
-            <Box sx={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100vw',
-                height: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.2)',
-                backdropFilter: 'blur(2px)',
-                zIndex: 9999
-            }}>
-                <CircularProgress />
-            </Box>
-        </div>
+        <Box sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            backdropFilter: 'blur(2px)',
+            zIndex: 9999
+        }}>
+            <CircularProgress />
+        </Box>
     );
 }

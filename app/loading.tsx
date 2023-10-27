8a66ef04ed4fbcabf2dea60aeac693be3eb28a5d
trisbt <tristanbott30@gmail.com>
2023-10-27 16:04:04 -0700

    add gradient to loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,20 +2,22 @@ import { CircularProgress, Box } from "@mui/material";
 
 export default function Loading() {
     return (
-        <Box sx={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-            backdropFilter: 'blur(2px)',
-            zIndex: 9999
-        }}>
-            <CircularProgress />
-        </Box>
+        <div className='song-page-main background-gradient min-h-[150em] min-w-[100vw]'>
+            <Box sx={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                backdropFilter: 'blur(2px)',
+                zIndex: 9999
+            }}>
+                <CircularProgress />
+            </Box>
+        </div>
     );
 }

e4bdccdde867e124cfb22c11144e2619772b03b2
trisbt <tristanbott30@gmail.com>
2023-12-13 22:41:10 -0800

    allow google to robots install redis/upstash

--- app/server_components/GetSpotifyById.tsx
+++ app/server_components/GetSpotifyById.tsx
@@ -1,7 +1,32 @@
 import GetAccessToken from "./GetAccessToken";
 import GetSpotifyAdvancedAudio from "./GetSpotifyAdvancedAudio";
+// import { Redis } from '@upstash/redis';
+// import { Ratelimit } from '@upstash/ratelimit';
+// import { request } from "http";
+// import { NextResponse } from "next/server";
+
+// const rateLimit = new Ratelimit({
+//     redis: Redis.fromEnv(),
+//     limiter: Ratelimit.slidingWindow(1, '5 s'),
+// })
+
+
 
 const GetSpotifyById = async (id: string) => {
+    // //check ip at redis
+    // const ip = request.headers.get('x-forwarded-for') ?? '';
+    // const { success, reset } = await ratelimit.limit(ip);
+    // if(!success){
+    //     const now = Date.now();
+    //     const retryAfter = Math.floor((reset-now) / 1000);
+    //     return new NextResponse('Too Many Requests', {
+    //         status: 429,
+    //         headers: {
+    //             ['retry-after']: `${retryAfter},`
+    //         },
+    //     })
+    // }
+
     const token = await GetAccessToken();
 
     const mainRes = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
